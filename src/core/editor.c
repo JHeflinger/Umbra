20d8ab4fed81a8538b86e3ed2bfa96b0fa5bc69b
Jason Heflinger <jasonheflinger@gmail.com>
2024-09-16 16:00:04 -0400

    more editor changes and updates

--- src/core/editor.c
+++ src/core/editor.c
@@ -46,17 +46,40 @@ int g_sub_divider = 400;
 int g_mouse_clicked_divider = 0;
 int g_mouse_clicked_sub_divider = 0;
 int g_focused_window = VIEWPORT_FOCUSED;
+int g_tr_semi_focused = EXPLORER_FOCUSED;
+int g_br_semi_focused = PROFILER_FOCUSED;
 
 void DrawOverlay() {
 
     DrawRectangle(0, 0, GetScreenWidth(), NAMEBAR_HEIGHT, GRAY_1);
     DrawRectangle(0, NAMEBAR_HEIGHT, GetScreenWidth(), DIVIDER_WIDTH, GRAY_2);
+
+    DrawRectangle(g_central_divider + DIVIDER_WIDTH, g_sub_divider + DIVIDER_WIDTH + NAMEBAR_HEIGHT, GetScreenWidth() - g_central_divider, NAMEBAR_HEIGHT, GRAY_1);
+    DrawRectangle(g_central_divider + DIVIDER_WIDTH, g_sub_divider + DIVIDER_WIDTH + NAMEBAR_HEIGHT + NAMEBAR_HEIGHT, GetScreenWidth() - g_central_divider, DIVIDER_WIDTH, GRAY_2);
     
     DRAW_TAB(5, 5, 70, NAMEBAR_HEIGHT - 5, GRAY_2);
-    DRAW_TAB(g_central_divider + 5, 5, 70, NAMEBAR_HEIGHT - 5, GRAY_2);
+    if (g_tr_semi_focused == EXPLORER_FOCUSED) {
+        DRAW_TAB(g_central_divider + 5, 5, 70, NAMEBAR_HEIGHT - 5, GRAY_2);
+    } else if (g_tr_semi_focused == EDITOR_FOCUSED) {
+        DRAW_TAB(g_central_divider + 5 + 70 + 5, 5, 60, NAMEBAR_HEIGHT - 5, GRAY_2);
+    } else {
+        printf("bad top-right focus value detected\n");
+        exit(1);
+    }
+    if (g_br_semi_focused == PROFILER_FOCUSED) {
+        DRAW_TAB(g_central_divider + 5, 5 + g_sub_divider + NAMEBAR_HEIGHT, 65, NAMEBAR_HEIGHT - 5, GRAY_2);
+    } else if (g_br_semi_focused == CHAIN_FOCUSED) {
+        DRAW_TAB(g_central_divider + 5 + 65 + 5, 5 + g_sub_divider + NAMEBAR_HEIGHT, 110, NAMEBAR_HEIGHT - 5, GRAY_2);
+    } else {
+        printf("bad bottom-right focus value detected\n");
+        exit(1);
+    }
 
     DrawText("Viewport", 10, 10, 14, g_focused_window == VIEWPORT_FOCUSED ? YELLOW : GRAY_X);
     DrawText("Explorer", g_central_divider + 10 + DIVIDER_WIDTH, 10, 14, g_focused_window == EXPLORER_FOCUSED ? YELLOW : GRAY_X);
+    DrawText("Editor", g_central_divider + 10 + DIVIDER_WIDTH + 70 + 10, 10, 14, g_focused_window == EDITOR_FOCUSED ? YELLOW : GRAY_X);
+    DrawText("Profiler", g_central_divider + 10 + DIVIDER_WIDTH, g_sub_divider + NAMEBAR_HEIGHT + 10, 14, g_focused_window == PROFILER_FOCUSED ? YELLOW : GRAY_X);
+    DrawText("Shader Chain", g_central_divider + 10 + DIVIDER_WIDTH + 65 + 10, g_sub_divider + NAMEBAR_HEIGHT + 10, 14, g_focused_window == CHAIN_FOCUSED ? YELLOW : GRAY_X);
 
     DRAW_VERTICAL_DIVIDER();
     DRAW_HORIZONTAL_DIVIDER();
@@ -71,10 +94,31 @@ void DrawOverlay() {
     g_central_divider = g_central_divider < MIN_CONFIG_WIDTH ? MIN_CONFIG_WIDTH : (g_central_divider > GetScreenWidth() - MIN_CONFIG_WIDTH ? GetScreenWidth() - MIN_CONFIG_WIDTH : g_central_divider);
     g_sub_divider = g_sub_divider < 0 ? 0 : (g_sub_divider > GetScreenHeight() - NAMEBAR_HEIGHT ? GetScreenHeight() - NAMEBAR_HEIGHT : g_sub_divider);
 
-    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), (Rectangle){ 0, 0, g_central_divider, GetScreenHeight() }))
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), (Rectangle){ 0, 0, g_central_divider, GetScreenHeight() })) {
         g_focused_window = VIEWPORT_FOCUSED;
-    else if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), (Rectangle){ g_central_divider, 0, GetScreenWidth() - g_central_divider, GetScreenHeight() }))
+    } else if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), (Rectangle){ g_central_divider, NAMEBAR_HEIGHT, GetScreenWidth() - g_central_divider, g_sub_divider })) {
+        if (g_tr_semi_focused == EXPLORER_FOCUSED)
+            g_focused_window = EXPLORER_FOCUSED;
+        else g_focused_window = EDITOR_FOCUSED;
+    } else if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), (Rectangle){ g_central_divider, NAMEBAR_HEIGHT + NAMEBAR_HEIGHT + g_sub_divider, GetScreenWidth() - g_central_divider, GetScreenHeight() - (NAMEBAR_HEIGHT + NAMEBAR_HEIGHT + g_sub_divider) })) {
+        if (g_br_semi_focused == PROFILER_FOCUSED)
+            g_focused_window = PROFILER_FOCUSED;
+        else g_focused_window = CHAIN_FOCUSED;
+    }
+
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), (Rectangle){ g_central_divider + 5, 5, 70, NAMEBAR_HEIGHT - 5 })) {
+        g_tr_semi_focused = EXPLORER_FOCUSED;
         g_focused_window = EXPLORER_FOCUSED;
+    } else if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), (Rectangle){ g_central_divider + 5 + 70 + 5, 5, 60, NAMEBAR_HEIGHT - 5 })) {
+        g_tr_semi_focused = EDITOR_FOCUSED;
+        g_focused_window = EDITOR_FOCUSED;
+    } else if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), (Rectangle){ g_central_divider + 5, 5 + g_sub_divider + NAMEBAR_HEIGHT, 65, NAMEBAR_HEIGHT - 5 })) {
+        g_br_semi_focused = PROFILER_FOCUSED;
+        g_focused_window = PROFILER_FOCUSED;
+    } else if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), (Rectangle){ g_central_divider + 5 + 65 + 5, 5 + g_sub_divider + NAMEBAR_HEIGHT, 110, NAMEBAR_HEIGHT - 5 })) {
+        g_br_semi_focused = CHAIN_FOCUSED;
+        g_focused_window = CHAIN_FOCUSED;
+    }
 
     static float alpha = 0.0f;
     if (IsKeyDown(KEY_LEFT_ALT))
@@ -89,7 +133,8 @@ void DrawOverlay() {
 
 void DrawPanels() {
     DrawViewport(0, NAMEBAR_HEIGHT, g_central_divider, GetScreenHeight() - NAMEBAR_HEIGHT);
-	DrawExplorer(g_central_divider + DIVIDER_WIDTH, NAMEBAR_HEIGHT, GetScreenWidth() - g_central_divider - DIVIDER_WIDTH, g_sub_divider);
+    if (g_tr_semi_focused == EXPLORER_FOCUSED)
+	    DrawExplorer(g_central_divider + DIVIDER_WIDTH, NAMEBAR_HEIGHT, GetScreenWidth() - g_central_divider - DIVIDER_WIDTH, g_sub_divider);
 }
 
 void RunEditor() {
--- src/panels/explorer.c
+++ src/panels/explorer.c
@@ -6,13 +6,12 @@ ARRLIST_PathString g_shader_paths = { 0 };
 
 void InitializeExplorer() {
     PopulateFilePaths(&g_shader_paths, ".c", ".");
-    for (int i = 0; i < g_shader_paths.size; i++)
-        printf("%s\n", ARRLIST_PathString_get(&g_shader_paths, i).raw);
 }
 
 void DrawExplorer(float x, float y, float w, float h) {
 	for (int i = 0; i < g_shader_paths.size; i++) {
-		DrawText(ARRLIST_PathString_get(&g_shader_paths, i).raw, x + 10, y + 10 * (20*i), 18, RAYWHITE);
+		int ypos = y + 10 + (20*i);
+		if (ypos < y + h)
+			DrawText(ARRLIST_PathString_get(&g_shader_paths, i).raw, x + 10, ypos, 14, RAYWHITE);
 	}
-	DrawRectangle(x, y, w, h, RED);	
 }

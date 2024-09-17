f6024a65cf92e0fa7ac705f6850879edc76e4881
Jason Heflinger <jasonheflinger@gmail.com>
2024-09-17 16:32:50 -0400

    implemented updated chaining from explorer

--- src/panels/chain.c
+++ src/panels/chain.c
@@ -8,16 +8,29 @@ void UpdateChain(ARRLIST_PathString* paths) {
 	for (int i = 0; i < paths->size; i++) {
 		int found = 0;
 		PathString str = ARRLIST_PathString_get(paths, i);
-		if (str.active == 0) continue;
-		for (int j = 0; j < g_active_shaders.size; j++) {
-			PathString cmpstr = ARRLIST_PathString_get(&g_active_shaders, j);
-			if (strcmp(cmpstr.raw, str.raw) == 0) {
-				found = 1;
-				break;
+		if (str.active == 1) {
+			for (int j = 0; j < g_active_shaders.size; j++) {
+				PathString cmpstr = ARRLIST_PathString_get(&g_active_shaders, j);
+				if (strcmp(cmpstr.raw, str.raw) == 0) {
+					found = 1;
+					break;
+				}
 			}
+			if (found == 0)
+				ARRLIST_PathString_add(&g_active_shaders, str);
+		} else {
+			int removal = -1;
+			for (int j = 0; j < g_active_shaders.size; j++) {
+				PathString cmpstr = ARRLIST_PathString_get(&g_active_shaders, j);
+				if (strcmp(cmpstr.raw, str.raw) == 0) {
+					found = 1;
+					removal = j;
+					break;
+				}
+			}
+			if (found == 1)
+				ARRLIST_PathString_remove(&g_active_shaders, removal);
 		}
-		if (found == 0)
-			ARRLIST_PathString_add(&g_active_shaders, str);
 	}
 }
 
